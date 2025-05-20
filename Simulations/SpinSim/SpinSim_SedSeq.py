c99bdb17d4acfdb22c9b59d8497efaf19e9a4b74
Alexandre Kaizeler <95570656+A-Kaizeler-A@users.noreply.github.com>
2025-05-20 17:25:26 +0100

    Commented SpinSim_SedSeq.py

--- Simulations/SpinSim/SpinSim_SedSeq.py
+++ Simulations/SpinSim/SpinSim_SedSeq.py
@@ -5,8 +5,8 @@ import time, os
 starttime = time.time()
 
 # Load data
-OGtable = pd.read_table("SG2017_WC1.txt", header=0)  # Expression table (TPM values)
-SedSeqTable = pd.read_table("YeastSs.txt", header=0, sep=" ")  # Molecular weights
+OGtable = pd.read_table("SG2017_WC1.txt", header=0)  # Expression table (TPMs) from which to start centrifugation simulation
+SedSeqTable = pd.read_table("YeastSs.txt", header=0, sep=" ")  # Table with Yeast Molecular RNAs. Replace with Human file, if needed
 
 # Centrifugation parameters
 CentrifugationN = 3 # Number of centrifugations performed
@@ -19,12 +19,12 @@ def spinner(weight_table):
     # Compute S using np.where based on 'Cromo' values
     S = np.where(
         weight_table['Cromo'] == 'Mito',
-        np.cbrt((weight_table['Length'])**(1/3)) * 3.23e-11 + 1.17e-9,
-        np.cbrt((weight_table['Length'])**(1/3)) * 6.09e-11 + 3.88e-10
+        np.cbrt((weight_table['Length'])**(1/3)) * 3.23e-11 + 1.17e-9, # Formula to apply in mitochondrial RNAs
+        np.cbrt((weight_table['Length'])**(1/3)) * 6.09e-11 + 3.88e-10 # Formula to apply to other RNAs
     )
 
     # Calculate 'd' using S and store it in the dataframe
-    weight_table['d'] = (S * Acceleration * Time) / TubeSize
+    weight_table['d'] = (S * Acceleration * Time) / TubeSize # To calculate percentage of each RNA that precipitates
 
     return weight_table
 if __name__ == "__main__":
@@ -36,12 +36,12 @@ if __name__ == "__main__":
     merged = OGtable.merge(WeightTable[['Gene', 'd']], left_on='GeneID', right_on='Gene')
 
     # Multiply TPM by d, CentrifugationN times
-    for _ in range(CentrifugationN):
+    for _ in range(CentrifugationN): #To multiply by the number of centrifugations performed
         merged['TPM'] = merged['TPM'] * merged['d']
 
     # Save result
-    output_path = "./SimulatedSamples/SG2017/SG2017_MimiSEQ_SedSeq_SG1.csv"
+    output_path = "./SimulatedSamples/SG2017/SG2017_MimiSEQ_SedSeq_SG1.csv" #To name and save our simulated file
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     merged.to_csv(output_path, index=False)
 
-    print('That took {} seconds'.format(time.time() - starttime))
+    print('That took {} seconds'.format(time.time() - starttime)) # For benchmarking purposes
